c456f1c5cbaa298604d296bad7afe1ffff4ab879
Sohel Amin <sohelamincse@gmail.com>
2016-01-04 22:49:39 +0600

    Minor changes

--- index.php
+++ index.php
@@ -1,45 +1,43 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>jQuery Ajax File Upload</title>
-</head>
-<body>
-	<form id="ajax_form" name="ajax_form" action="process.php" enctype="multipart/form-data" method="post">
-		Name: <input type="text" name="name" required /><br />
-		Image: <input type="file" name="image" required /><br />
-		<input type="submit" name="ajax_form_submit" id="ajax_form_submit" value="SAVE" />	
-	</form>
-	<script src="js/jquery.min.js"></script>
-	<script>
-		$(document).ready(function(){
-			$('input#ajax_form_submit').bind("click",function(e) { 
-				$('form#ajax_form_submit').submit();				
-			});	
+    <head>
+        <meta charset="UTF-8">
+        <title>jQuery Ajax File Upload</title>
+    </head>
+    <body>
+        <form id="ajax_form" name="ajax_form" action="process.php" enctype="multipart/form-data" method="post">
+            <label>Choose a file: </label><input type="file" name="image" required /><br />
+            <input type="submit" name="upload" value="Upload">
+        </form>
 
-			$('form#ajax_form').on('submit', function(e){
-				e.stopPropagation();
-				e.preventDefault();	
-				$form = $('form#ajax_form');
-				var formObj = $(this);
-				var formURL = formObj.attr("action");
-				var formData = new FormData(this);				
-				$.ajax({
-					type: 'POST',
-					url: formURL,
-					data: formData,
-					processData: false, 
-					contentType: false
-				})
-				.done(function( msg ) {
-						if ( msg == 1 ) {
-							alert('Success');
-						} else {
-							alert('Something went wrong!');
-						}	
-				});
-			});			
-		});
-	</script>
-</body>
+        <script src="js/jquery.min.js"></script>
+        <script>
+            $(document).ready(function() {
+
+                $('form#ajax_form').on('submit', function(e) {
+                    e.preventDefault();
+
+                    var formObj = $(this);
+                    var formURL = formObj.attr("action");
+                    var formData = new FormData(this);
+
+                    $.ajax({
+                        type: 'POST',
+                        url: formURL,
+                        data: formData,
+                        processData: false,
+                        contentType: false
+                    })
+                    .done(function(data) {
+                        if (data == 1) {
+                            alert('Success');
+                        } else {
+                            alert('Something went wrong!');
+                        }
+                    });
+                });
+
+            });
+        </script>
+    </body>
 </html>
--- process.php
+++ process.php
@@ -1,11 +1,10 @@
-<?php 
-if ( isset ($_POST['name']) ) {
-	$is_uploaded = move_uploaded_file( $_FILES['image']['tmp_name'], 'upload/' . $_FILES['image']['name'] );
+<?php
+if (isset($_POST['upload'])) {
+    $is_uploaded = move_uploaded_file($_FILES['image']['tmp_name'], 'uploads/' . $_FILES['image']['name']);
 
-	if ( $is_uploaded ) {
-		echo 1;
-	} else {
-		echo 0;
-	}
+    if ($is_uploaded) {
+        echo 1;
+    } else {
+        echo 0;
+    }
 }
-?>
